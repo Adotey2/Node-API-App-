350c1ba409a8621b86589d1bbcde544e37cb66b7
Adotey2 <alloteylesie8@example.com>
2024-04-08 18:02:36 +0000

    added nodemon and connected to database

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express=require('express')
+const mongoose = require('mongoose')
 const app =express()
 
 app.get('/',(req,res)=>{
     res.send('Hello Node API')
  })
 
-app.listen(3000,()=>{
-console.log("App is running on port 3000")
+app.get('/blog',(req,res)=>{
+    res.send('Hello blog my name is Leslie')
+})
+
+mongoose.
+connect('mongodb+srv://Leslie:leslie123@cluster0.xoqp18x.mongodb.net/Node-API?retryWrites=true&w=majority&appName=Cluster0')
+.then(()=>{
+    console.log('connected to MongoDB')
+    app.listen(3000,()=>{
+        console.log("App is running on port 3000")
+        });
+    
+}).catch((error)=>{
+    console.log(error)
 })
